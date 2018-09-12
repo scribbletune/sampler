cba47680c06ab4509dad2d2ae098b4bc99183897
Walmik Deshpande <walmik.deshpande@gmail.com>
2018-09-12 14:52:23 -0700

    Add example

--- example/script.js
+++ example/script.js
@@ -1,6 +1,14 @@
+var key = 'F';
+var scale = 'minor';
+
+const getNotes = (key, octave, scale) => {
+	var scale = scribble.scale(key + octave + ' ' + scale);
+	var scaleSliced = scale.slice(0, 3);
+	return [...scaleSliced, ...scaleSliced.reverse(), ...scaleSliced, ...scale.slice(4).reverse()];
+}
 scribble.clip({
-	notes: scribble.scale('F4 minor').slice(0, 3),
-	pattern: '[xxx]',
+	notes: getNotes(key, 3, scale),
+	pattern: '[xx]',
 	// In the following method '/samples/' is explicitly set as the second argument
 	// because we are using a custom location for it
 	// In most cases you ll just clone this repo in your public/ folder and if you did that, 
@@ -8,6 +16,29 @@ scribble.clip({
 	sampler: getSampler('korgBass', '/samples/')
 }).start();
 
+scribble.clip({
+	notes: getNotes(key, 4, scale),
+	pattern: 'xx[-x]'.repeat(3) + 'xx[xx]',
+	// In the following method '/samples/' is explicitly set as the second argument
+	// because we are using a custom location for it
+	// In most cases you ll just clone this repo in your public/ folder and if you did that, 
+	// the default location would be used and you wont need to specify the second argument
+	sampler: getSampler('superSaw', '/samples/')
+}).start();
+
+
+scribble.clip({
+	notes: scribble.progression(key + '3 ' + scale, 'i VII VI VII'),
+	pattern: 'x-xx-',
+	// In the following method '/samples/' is explicitly set as the second argument
+	// because we are using a custom location for it
+	// In most cases you ll just clone this repo in your public/ folder and if you did that, 
+	// the default location would be used and you wont need to specify the second argument
+	sampler: getSampler('piano', '/samples/')
+}).start();
+
+
+Tone.Transport.bpm.value = 145;
 document.querySelector('#startBtn').addEventListener('click', function() {
 	Tone.Transport.start();
 });
